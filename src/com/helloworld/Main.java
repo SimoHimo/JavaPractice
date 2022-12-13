61e6c41b43caf163acb6557cacacec226887d6c2
SimoHimo <xsimohimox@gmail.com>
2022-12-13 21:09:55 +0600

    first java git

---
+++ src/com/helloworld/Main.java
@@ -0,0 +1,12 @@
+package com.helloworld;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 30;
+        Date now = new Date();
+
+        System.out.println(now);
+    }
+}
